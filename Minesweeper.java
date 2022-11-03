5284134d57c5a5d0a0a17e7851eda547236f00d5
micromashor <micromashor@gmail.com>
2022-11-03 11:38:01 -0500

    add Minesweeper and MinesweeperGame java source files

---
+++ Minesweeper.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Minesweeper {
+        public static void main(String[] args) {
+                System.out.println("lmao");
+        }
+}
---
+++ MinesweeperGame.java
@@ -0,0 +1,9 @@
+public class MinesweeperGame {
+
+
+
+
+        public MinesweeperGame(int dimension_x, int dimension_y, int mine_count) {
+                
+        }
+}
