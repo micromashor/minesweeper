217375d062569cbe937acd9693dd45a13b3cf736
micromashor <micromashor@gmail.com>
2022-11-07 10:54:16 -0600

    work

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -1,9 +1,33 @@
 public class MinesweeperGame {
+        public class Mine {
+                public int data;
+                public boolean isHidden() {
+                        return false;
+                }
+                public boolean reveal() {
 
+                }
+        }
 
+        private int[][] grid;
 
+        public MinesweeperGame(int rows, int cols, int mine_count) {
+                grid = new int[rows][cols];
 
-        public MinesweeperGame(int dimension_x, int dimension_y, int mine_count) {
-                
+                for (int x = 0; x < grid.length; x++) {
+                        for (int y = 0; y < grid[0].length; y++) {
+                                grid[x][y] = 0;
+                        }
+                }
+        }
+
+        /* Accepts coordinates (x, y) and returns whether the box is already touched */
+        public boolean reveal(int x, int y) {
+                return false;
+        }
+
+        /* Accepts coordinates (x, y) and returns whether a flag is set on that space */
+        public boolean setFlag(int x, int y) {
+                return true;
         }
 }
